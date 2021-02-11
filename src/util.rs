cc5d9d02de0b4288ed544292fe2e0f8b3edcccd3
WilliButz <willibutz@posteo.de>
2021-02-11 19:28:56 +0100

    move functionality from main to separate modules

---
+++ src/util.rs
@@ -0,0 +1,18 @@
+use crate::config::Config;
+
+use uuid::Uuid;
+use std::fs;
+
+pub fn append_temp_suffix(s: &str) -> String {
+    format!("{}.part", s)
+}
+
+pub fn new_random_uuid() -> String {
+    Uuid::new_v4().to_string()
+}
+
+pub fn cleanup(config: &Config) {
+    if let Err(_) = fs::remove_file(config.socket_path.to_string()) {
+        eprintln!("failed to cleanup socket {}", config.socket_path);
+    }
+}
